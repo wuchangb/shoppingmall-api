42e1aefa105f92da2f96794ad5c434576778c03a
wuchang <kkang0820qus@gmail.com>
2023-04-20 10:23:23 +0900

    model - User ì생성

--- src/main/java/com/example/cwshopping/model/user/User.java
+++ src/main/java/com/example/cwshopping/model/user/User.java
@@ -1,6 +1,44 @@
 package com.example.cwshopping.model.user;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_tb")
+@Setter
+@Getter
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private Boolean status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(Long id, String username, String password, String email, String role, Boolean statud, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+
+    }
 }
